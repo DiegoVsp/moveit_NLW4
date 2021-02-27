4586ab67b5ca2a01fec2a51c60075da71815adaa
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-02-27 13:32:52 -0300

    finalizando aula 3

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -2,21 +2,20 @@ import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/ChallengeBox.module.css";
 export function ChallengeBox() {
-const contextData = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
-  const hasActiveChallenge = true;
   return (
     <div className={styles.ChallengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount}xp</header>
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg `} />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
           <footer>
-            <button type="button" className={styles.challengeFailedButton}>
+            <button type="button" className={styles.challengeFailedButton} onClick={resetChallenge}>
               Falhei
             </button>
             <button type="button" className={styles.challengeSucceededButton}>
@@ -25,14 +24,14 @@ const contextData = useContext(ChallengesContext);
           </footer>
         </div>
       ) : (
-          <div className={styles.challengeNotActive}>
-            <strong>Finalize um ciclo para receber um desafio</strong>
-            <p>
-              <img src="icons/level-up.svg" alt="Level Up" />
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
             Avance de level completando desafios.
           </p>
-          </div>
-        )}
+        </div>
+      )}
     </div>
   );
 }
